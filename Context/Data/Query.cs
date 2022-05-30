780916145b5f43804e86124231dd71248855ce68
medarametlaseshagiri <medarametla.seshagiri@gmail.com>
2022-05-30 18:44:52 -0400

    first commit

---
+++ Context/Data/Query.cs
@@ -0,0 +1,12 @@
+﻿using GraphQLSample.Model;
+namespace GraphQLSample.Context.Data
+{
+    public class Query
+    {
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<Project> GetProjects([Service] ApplicationDbContext context) =>
+            context.Projects;
+    }
+}
